3cc5b8eb4e7180cffc5463e1cb1fd59ccedda2fa
Kyle Balnave <balnavekyle@gmail.com>
2014-01-08 22:22:34 +0000

    Modified unit test to fake HTTPConnection

--- test/com/balnave/rambler/RunnerTest.java
+++ test/com/balnave/rambler/RunnerTest.java
@@ -6,7 +6,44 @@
 package com.balnave.rambler;
 
 import com.balnave.rambler.queue.QueueItem;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
 import junit.framework.TestCase;
+
+class SimpleServer {
+
+    public static HttpServer server;
+
+    public static void start() throws Exception {
+        server = HttpServer.create(new InetSocketAddress("127.0.0.1", 7777), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    public static void stop() {
+        server.stop(1);
+        server = null;
+    }
+
+    static class MyHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello World!";
+            byte[] bytes = response.getBytes();
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, bytes.length);
+            exchange.getResponseBody().write(bytes);
+            exchange.close();
+        }
+
+    }
+}
 
 /**
  *
@@ -21,19 +58,22 @@ public class RunnerTest extends TestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        SimpleServer.start();
+
     }
 
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
+        SimpleServer.stop();
     }
 
     /**
      * Test of call method, of class Runner.
      */
-    public void testCall() throws Exception {
-        System.out.println("call");
-        Runner instance = new Runner(new QueueItem("http://www.google.com/"));
+    public void testCall200() throws Exception {
+        System.out.println("testCall200");
+        Runner instance = new Runner(new QueueItem("http://127.0.0.1:7777"));
         int expResult = 200;
         Result result = instance.call();
         assertEquals(expResult, result.getResponseStatus());
@@ -44,7 +84,7 @@ public class RunnerTest extends TestCase {
      */
     public void testGetResult() throws Exception {
         System.out.println("getResult");
-        Runner instance = new Runner(new QueueItem("http://www.google.com/"));
+        Runner instance = new Runner(new QueueItem("http://127.0.0.1:7777/"));
         Result result = instance.call();
         Result expResult = instance.getResult();
         assertEquals(expResult, result);
